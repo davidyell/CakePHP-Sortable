3c9b436eb14d22cf9020e639a6bea8209f12fe01
David Yell <dyell@ukwebmedia.com>
2014-06-26 09:26:02 +0100

    Created some default values to make the component more flexible. Fixed a bug in the settings merge in the constructor which wasn't preserving the defaults. Made NiceAdmin optional, but on by default.

--- Controller/Component/SortableComponent.php
+++ Controller/Component/SortableComponent.php
@@ -14,7 +14,12 @@ class SortableComponent extends Component {
  *
  * @var array
  */
-    public $settings = array();
+    public $settings = array(
+		'model' => '',
+		'field' => 'id',
+		'sortField' => 'sortorder',
+		'useNiceAdmin' => true
+	);
 
 /**
  * Constructor
@@ -23,13 +28,13 @@ class SortableComponent extends Component {
  * @param array $settings Array of configuration settings.
  */
     public function __construct(ComponentCollection $collection, $settings = array()) {
+		parent::__construct($collection, $this->settings);
+		
         // Set the default to the current model, unless overridden
-        $this->settings = array(
-            'model' => $collection->getController()->modelClass
-        );
-        $settings = array_merge($this->settings, $settings);
-
-        parent::__construct($collection, $settings);
+		if (!isset($settings['model'])) {
+			$this->settings['model'] = $collection->getController()->modelClass;
+		}
+        $this->settings = array_merge($this->settings, $settings);
     }
 
 /**
@@ -44,20 +49,25 @@ class SortableComponent extends Component {
 
         if ($controller->request->is('post') && !empty($postedRanks)) {
             $ranks = explode(',', $postedRanks[0]);
-
+			
             $i = 0;
             foreach ($ranks as $order => $rowId) {
-                $data[$i][$this->settings['model']]['id'] = $rowId;
-                $data[$i][$this->settings['model']]['sortorder'] = $order + 1; // As arrays count from 0, but our ranks count from 1
+                $data[$i][$this->settings['model']][$this->settings['field']] = $rowId;
+                $data[$i][$this->settings['model']][$this->settings['sortField']] = $order + 1; // As arrays count from 0, but our ranks count from 1
                 $i++;
             }
+			
+			$element = 'default';
+			if ($this->settings['useNiceAdmin']) {
+				$element = 'NiceAdmin.alert-box';
+			}
 
             $controller->loadModel($this->settings['model']);
             if ($controller->{$this->settings['model']}->saveAll($data, array('validate' => false))) {
-                $controller->Session->setFlash($this->settings['model'].' order updated', 'NiceAdmin.alert-box', array('class' => 'alert-success'));
+                $controller->Session->setFlash($this->settings['model'].' order updated', $element, array('class' => 'alert-success'));
                 $controller->redirect($controller->referer());
             } else {
-                $controller->Session->setFlash($this->settings['model'].' order could not be updated, please try again', 'NiceAdmin.alert-box', array('class' => 'alert-error'));
+                $controller->Session->setFlash($this->settings['model'].' order could not be updated, please try again', $element, array('class' => 'alert-error'));
             }
         }
     }
